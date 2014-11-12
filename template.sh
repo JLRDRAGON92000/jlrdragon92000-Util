679a2c2f6537d6ed1de92efb7cf46e194ab98212
Jared L. Rankin <j.rankin@ctec.clark.edu>
2014-11-12 13:51:25 -0800

    updated template: can now copy stylesheet

--- template.sh
+++ template.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Copy Util/template.html to the current directory, and optionally copy the stylesheet (template.css) to a specified directory relative to the current one
+
+# Constants for the locations
 TEMPLATE_FPATH="$UTIL_DIR/template.html";
+TEMPLATE_CSS_FPATH="$UTIL_DIR/template.css";
+
+# Copy the HTML
 cp "$TEMPLATE_FPATH" "./$1";
 
+# If the user specifies, copy the stylesheet
+if [ -n "$2" ];
+then
+	cp "$TEMPLATE_CSS_FPATH" "./$2";
+fi
