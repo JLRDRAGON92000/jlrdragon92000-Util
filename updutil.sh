79c5bdfe87a73ee37e5ebc57f530e5e38a2e0bcd
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-11-12 20:21:04 -0800

    first attempt at an auto updater that downloads from this repository

--- updutil.sh
+++ updutil.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
+# Update Util/
+
+# Remote URL of the git repository
+FETCH_REPO_URL="https://github.com/JLRDRAGON92000/jlrdragon92000-Util.git";
+# Working directory while new Util/ is merged
+UTIL_TMP_DIR="$HOME/Util.tmp";
+
 # Get first argument
 # If it is nil, use * (Everything)
 CFIL="$1";
+
+# Fetch mode (Fetch from the git repository)
+if [ "$CFIL" == "--fetch" ];
+then
+	FFIL="$2";
+	if [ -z "$2" ];
+	then
+		:;
+	fi
+	
+	# Save the user's current working directory
+	pwdbak="$PWD";
+	# Go to Util/
+	cd "$UTIL_DIR";
+	# Get the list of different files from the upstream
+	UTIL_DIFF_LIST=$(git diff --name-only @{upstream} "$FFIL");
+	# Download the most current version of Util/ from the repo
+	cd "$HOME";
+	git clone "$FETCH_REPO_URL";
+	mv "jlrdragon92000-Util" "Util.tmp";
+	# Copy the changed files to Util/
+	for CFNAME in $UTIL_DIFF_LIST;
+	do
+		cp -f "Util.tmp/$CFNAME" "Util/$CFNAME"; 
+	done
+	# Clean up
+	rm -rf "Util.tmp";
+	cd "$pwdbak";
+fi
+
 if [ -z "$1" ];
 then
 	CFIL="*";
