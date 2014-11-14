27b27c294af09f789fa1b5cca25ee22dd8b4db5a
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-11-13 18:40:24 -0800

    vutil now locks writing while file is being viewed

--- vutil.sh
+++ vutil.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # View a Util script in chosen viewer
-VUTIL_VIEWER="less";
+VUTIL_VIEWER="vim";
 
-# Get object
+# Lock write permission temporarily
+chmod -w "$UTIL_DIR/$1.sh";
+# Invoke viewing application on target
 "$VUTIL_VIEWER" "$UTIL_DIR/$1.sh";
+# Unlock file write permission
+chmod u+w "$UTIL_DIR/$1.sh";
 
