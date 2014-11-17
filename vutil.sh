06580d769e2ab9f5f21c0c2720d708bce9c4a08a
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-11-17 10:51:31 -0800

    vutil now uses "view" as default viewer

--- vutil.sh
+++ vutil.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # View a Util script in chosen viewer
-VUTIL_VIEWER="vim";
+VUTIL_VIEWER="view";
 
-# Lock write permission temporarily
-chmod -w "$UTIL_DIR/$1.sh";
+# Lock write permission temporarily (no longer needed)
+# chmod -w "$UTIL_DIR/$1.sh";
 # Invoke viewing application on target
 "$VUTIL_VIEWER" "$UTIL_DIR/$1.sh";
-# Unlock file write permission
-chmod u+w "$UTIL_DIR/$1.sh";
+# Unlock file write permission (no longer needed)
+# chmod u+w "$UTIL_DIR/$1.sh";
 
