735bdd5a87762933806457cdb9d5960819264331
JLRDRAGON92000 <jlrdragon@gmail.com>
2014-12-10 00:35:54 -0800

    when using ainfo as root, now shows warning in prompt

--- .ps1custopts.lock/._X_jinfo.lock.sh
+++ .ps1custopts.lock/._X_jinfo.lock.sh
@@ -2,25 +2,26 @@
 # WARNING: This file is not intended for use with the PS1 mode system. It is used by the ainfo prompt to set the Java info prompt.
 dirbase="\\w";
 if [ -n "$USE_TITLEBAR" ]; then
-        if [ -z "$PROMPT_SHOW_SSH_UH" ]; then
-                titleb="\[\e]0;${dirbase}\007\]";
-        elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
-                titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
-        elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
-                titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
-        else
-                titleb="\[\e]0;${dirbase}\007\]";
-        fi
+	if [ -z "$PROMPT_SHOW_SSH_UH" ]; then
+		titleb="\[\e]0;${dirbase}\007\]";
+	
+	elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
+		titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+	elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
+		titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+	else
+		titleb="\[\e]0;${dirbase}\007\]";
+	fi
 else
-        if [ -z "$PROMPT_SHOW_SSH_UH" ]; then
-                titleb="";
-        elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
-                titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
-        elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
-                titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
-        else
-                titleb="";
-        fi
+	if [ -z "$PROMPT_SHOW_SSH_UH" ]; then
+		titleb="";
+	elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
+		titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+	elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
+		titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+	else
+		titleb="";
+	fi
 fi
 
 export promptcmd='
