d9894db74ae2800763edb3ee9b55deddaeb2c94c
JLRDRAGON92000 <jlrdragon@gmail.com>
2015-04-14 19:03:25 -0700

    fixed appearance of ainfo prompts for root

--- ps1opts/._X_cinfo.lock.sh
+++ ps1opts/._X_cinfo.lock.sh
@@ -13,21 +13,29 @@ if [ -n "$USE_TITLEBAR" ]; then
 	elif [ -z "$PROMPT_SHOW_SSH_UH" ]; then
 		titleb="\[\e]0;${dirbase}\007\]";
 	elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
-			titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
 	elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
-			titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		titleb="\[\e]0;\${USER}@\${HOSTNAME} : ${dirbase}\007\]\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
 	else
-			titleb="\[\e]0;${dirbase}\007\]";
+		titleb="\[\e]0;${dirbase}\007\]";
 	fi
 else
-	if [ -z "$PROMPT_SHOW_SSH_UH" ]; then
-			titleb="";
+	if [ $(id -u) -eq 0 ]; then
+		if [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
+			titleb="\[\e[01;37m\][\[\e[01;31m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
+			titleb="\[\e[01;37m\][\[\e[01;31m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		else
+			titleb="\[\e[01;37m\][\[\e[01;31m\]-ROOT-\[\e[01;37m\]]";
+		fi
+	elif [ -z "$PROMPT_SHOW_SSH_UH" ]; then
+		titleb="";
 	elif [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
-			titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
 	elif [ "$PROMPT_SHOW_SSH_UH" == "2" ]; then
-			titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
+		titleb="\[\e[01;37m\][\[\e[01;32m\]\${USER}@\${HOSTNAME}\[\e[01;37m\]]";
 	else
-			titleb="";
+		titleb="";
 	fi
 fi
 
