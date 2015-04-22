09f5d7e5992c2ff6049cf9395b4122591d21fc58
JLRDRAGON92000 <jlrdragon@gmail.com>
2015-04-21 17:34:18 -0700

    finished ainfo prompt rewrite; ainfo now sources a few common files rather than including duplicate code in multiple files

--- ps1opts/._X_ainfo_strap.sh
+++ ps1opts/._X_ainfo_strap.sh
@@ -1,6 +1,3 @@
-# WARNING: Not valid as PS1 mode
-# WARNING: This file is not intended for use with the PS1 mode system. It is used by the ainfo prompt to set the Java info prompt.
-dirbase="\\w";
 if [ -n "$USE_TITLEBAR" ]; then
 	if [ $(id -u) -eq 0 ]; then
 		if [ -n "$SSH_CLIENT" ] && [ "$PROMPT_SHOW_SSH_UH" == "1" ]; then
@@ -38,34 +35,17 @@ else
 		titleb="";
 	fi
 fi
+export titleb;
 
-if [ -n "$VIM" ]; then
-	vimsh="[\[\e[01;31m\]$SHLVL VIM\[\e[01;37m\]] ";
-elif [ $SHLVL -gt 1 ]; then
-	vimsh="[\[\e[01;31m\]$SHLVL SUB\[\e[01;37m\]] ";
+source "$PS1M_DIR/._X_showsshuh.sh";
+if [ -e "./.webproj.lock" ]; then
+	source "$PS1M_DIR/._X_winfo.sh";
+elif [ -e "./.javaproj.lock" ]; then
+	source "$PS1M_DIR/._X_jinfo.sh";
+elif [ -e "./.cproj.lock" ]; then
+	source "$PS1M_DIR/._X_cinfo.sh";
 else
-	vimsh="";
+	source "$PS1M_DIR/._X_ginfo.sh";
 fi
-
-export promptcmd='
-	statshow="[$pstattmp] ";
-	dirbase="\\w";
-	if [ -e "./.javaproj.lock" ]; then
-		lastbuild_time=$(cut -f1 -d"|" "./.javaproj.lock");
-		lastbuild_fil=$(cut -f2 -d"|" "./.javaproj.lock");
-		lastbuild_res=$(cut -f3 -d"|" "./.javaproj.lock");
-		csrcs=$(ls -a1 2>/dev/null | grep -c "\.java\$");
-		cclss=$(ls -a1 2>/dev/null | grep -c "\.class\$");
-		cfils=$(ls -al 2>/dev/null | grep -c "^-");
-		let cothr="cfils-(csrcs+cclss)";
-		cdirs=$(ls -Al 2>/dev/null | grep -c "^d");
-	fi;
-	if [ "$lastbuild_res" == "SUCCESS" ]; then
-		lastbuild_success="SUCCESS";
-		lastbuild_failure="";
-	else
-		lastbuild_success="";
-		lastbuild_failure="FAILURE";
-	fi;
-	PS1="$titleb\[\e[01;37m\][\[\e[01;34m\]\${debian_chroot:+(\$debian_chroot) }${dirbase}\[\e[01;37m\]][\[\e[01;33m\]\${csrcs}s \${cclss}c \${cothr}o \${cdirs}d\[\e[01;37m\]][\[\e[01;36m\]\$(date \"+%a %Y-%m-%d %H:%M\")\[\e[01;37m\]][\[\e[01;36m\]\${lastbuild_time} ${lastbuild_fil} \[\e[01;32m\]\${lastbuild_success}\[\e[01;31m\]\${lastbuild_failure}\[\e[01;37m\]]\$(__git_ps1 \"[\[\e[01;35m\]%s \$(__git_ps1_show_sha)\[\e[01;37m\]]\")\n\[\e[01;37m\]$vimsh\$(if [ -z \"\$SSH_IP\" ]; then echo \"LOCAL\"; else echo \$SSH_IP; fi;) -> \${TTY:5} \${statshow}\\\$ \[\e[00m\]";';
 eval "$promptcmd";
+
